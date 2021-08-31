c2a92f2c4e5705222f2cc3b5c261e97864577cda
jack <jack.7e@protonmail.com>
2021-08-31 18:58:04 +0100

    updated parser.py

--- parser.py
+++ parser.py
@@ -1,24 +1,12 @@
 import json
 
-UNSIGS_MINTED = 31119
-
-
-def csv_split(s):
-    # seperate items in a csv string and return in list format
-    return s.split(',')
 
 if __name__ == "__main__":
     # convert file to parsable data
-    my_file = open("unsig.json", "r")
-    data = my_file.readlines()
-    json_str = data[0].replace("'", "") # note removal of
-                                        # single quotation 
-                                        # needed to clean json
-    unsig_json = json.loads(json_str)
+    f = open("unsig.json", "r")
+    data = f.readlines()
+    unsig_json = json.loads(data[0])
 
     # parse data
     print(unsig_json[0])
     print(unsig_json[0][0])
-
-
-
