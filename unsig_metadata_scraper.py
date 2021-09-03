b923b1b5fcbeb8e33ea8364dcc1f00fd0160a063
jack <jack.7e@protonmail.com>
2021-09-03 13:54:58 +0100

    renamed the web scraper

---
+++ unsig_metadata_scraper.py
@@ -0,0 +1,93 @@
+from bs4 import BeautifulSoup
+from html.parser import HTMLParser
+from multiprocessing import Process, Manager, Pool
+import time
+import requests
+import json
+
+UNSIG_URL = "https://www.unsigs.com/details/"
+UNSIGS_MINTED = 31119
+OUT_FILE = "unsig.json"
+
+
+def find_between( s, first, last ):
+    # find a substring between two given strings
+    try:
+        start = s.index( first ) + len( first )
+        end = s.index( last, start )
+        return s[start:end]
+    except ValueError:
+        return ""
+
+def csv_split(s):
+    # separate items in a csv string and return in list format
+    return s.split(',')
+
+def unsig_job(i, unsig_list):
+    # scrapes given url for unsig data, creates a dict adds dict
+    # to a multiprocess list
+    
+    index = str("{:05d}".format(i)) # pad zeros in front of int
+    url = UNSIG_URL + index
+
+    # Make a GET request to fetch the raw HTML content
+    try:
+        # get the html for unsig 'i'
+        html_content = requests.get(url).text
+        soup = BeautifulSoup(html_content, "lxml")
+        res = soup.find_all("li")
+        res = str(res).replace("'","") # clean data by removing single quotations
+        # parse the html 
+        properties = csv_split(find_between(res, "This unsig NFT has <b>","</b>"))
+        colors = csv_split(find_between(res, "Colors:</b> [","]"))
+        distributions = csv_split(find_between(res, "Distributions:</b> [","]"))
+        rotations = csv_split(find_between(res, "Rotations:</b> [","]"))
+        multipliers = csv_split(find_between(res, "Multipliers:</b> [","]"))
+        # format the data as dict
+        data = [{'index': i},
+            {'properties': properties},
+            {'colors':colors},
+            {'distributions':distributions},
+            {'rotations':rotations},
+            {'multipliers':multipliers}
+            ]
+        unsig_list.append(data)
+        return
+    except:
+        # error wait and recursively try again
+        time.sleep(0.1)
+        unsig_job(i, unsig_list)
+
+if __name__ == "__main__":
+    
+    #lst_a = []
+    #unsig_job(26140, lst_a)
+    # multiprocessing vars
+    manager = Manager()
+    unsig_list = manager.list() # shared dict
+    
+    # run multiple process for the function unsig_job(_,_)
+    for i in range (0, UNSIGS_MINTED):
+        p = Process(target=unsig_job, args=(i, unsig_list))
+        p.start()
+        # wait 1/10 second to avoid socket errors and OS threading errors
+        time.sleep(0.1)
+        print("Progress " + str("{:05d}".format(len(unsig_list))) + "/" + str("{:05d}".format(UNSIGS_MINTED)),end="\r")
+     
+    # wait until all processes are done
+    while len(unsig_list) != UNSIGS_MINTED:
+        pass
+    
+    print("")
+
+    # convert multithreaded proxy list into standard list for json
+    unsig_list = list(unsig_list)
+    
+    # order the set (the multiprocessing muddles it up)
+    unsig_list = sorted(unsig_list, key=lambda i: i[0]['index'])
+
+    # save to file
+    with open(OUT_FILE, "w") as outfile:
+        json.dump(unsig_list, outfile)
+
+    print("finished... scraped unsig data saved to \"" + OUT_FILE + "\"")
